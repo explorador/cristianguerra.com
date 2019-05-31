da75b1d26ccc7c0142c6edb31edd72a1de9aa9e7
Cristian <cristianguerra@rocketmail.com>
2019-05-31 17:59:28 -0400

    Adding "styled-components"

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,46 +8,66 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
+import Container from "./container"
+import styled from "styled-components"
 import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
+/**
+ * Styles for site wrapper div.
+ */
+const SiteWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
+`
+
+/**
+ * Styles for main site element.
+ */
+const MainStyled = styled.main`
+	flex: 1 0 auto;
+`
+
+/**
+ * Layout Component
+ */
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+	<StaticQuery
+		query={graphql`
+			query SiteTitleQuery {
+				site {
+					siteMetadata {
+						title,
+						author_url
+					}
+				}
+			}
+		`}
+		render={data => (
+			<SiteWrapper>
+				<Header siteTitle={data.site.siteMetadata.title} />
+				<MainStyled>{children}</MainStyled>
+				<footer
+					style={{
+						padding: `1em 0`
+					}}
+				>
+					<Container
+						style={{
+							textAlign: `center`
+						}}
+					>
+						© {new Date().getFullYear()} {data.site.siteMetadata.author_url}
+					</Container>
+				</footer>
+			</SiteWrapper>
+		)}
+	/>
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
 
 export default Layout
