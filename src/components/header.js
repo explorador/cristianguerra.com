356fcf2c102b6dac879a4774cdddc7fe76da3b81
Cristian <cristianguerra@rocketmail.com>
2019-05-31 18:01:56 -0400

    <Header /> component main styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,38 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import MainMenu from "./mainmenu"
+import Container from "./container"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+	<header
+		style={{
+			marginBottom: `1.45rem`,
+		}}
+	>
+		<Container>
+			<h1 style={{ margin: 0 }}>
+				<Link
+					to="/"
+					style={{
+						textDecoration: `none`,
+					}}
+				>
+					{siteTitle}
+				</Link>
+			</h1>
+
+			<MainMenu />
+		</Container>
+	</header>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+	siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+	siteTitle: ``,
 }
 
 export default Header
