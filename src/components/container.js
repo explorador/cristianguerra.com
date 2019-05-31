a0379167a9accf32d8aaf2cd130b82baf9fbcd3d
Cristian <cristianguerra@rocketmail.com>
2019-05-31 19:40:52 -0400

    Adding mediumWidth size to <Container /> component

--- src/components/container.js
+++ src/components/container.js
@@ -1,8 +1,17 @@
 import styled from 'styled-components'
 
+/**
+ * Container sizes.
+ */
+const mediumWidth = `1200px`;
+const defaultWidth = `720px`;
+
 const Container = styled.div`
-	max-width: ${ props => props.fullWidth ? "100%" : "1200px" };
+	${ props => ( props.fullWidth && ! props.mediumWidth ) ? "max-width: 100%" : `max-width: ${defaultWidth};` };
+	${ props => props.mediumWidth && `max-width: ${mediumWidth};` };
 	margin-right: auto;
 	margin-left: auto;
+	padding-right: 1em;
+	padding-left: 1em;
 `
 export default Container
