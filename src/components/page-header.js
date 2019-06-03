d7fa066b76eb2428eec6d11251dcc7d527b65761
Cristian <cristianguerra@rocketmail.com>
2019-06-02 21:02:08 -0400

    <PageHeader /> component styles

--- src/components/page-header.js
+++ src/components/page-header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import styled from "styled-components"
+import { rem } from "polished"
+
+const PageHeaderWrapper = styled.div`
+	padding-top: ${ props => rem( 100, props.theme.baseFontSize ) };
+`
 
 const PageHeader = ({ children }) => (
-	<>
+	<PageHeaderWrapper>
 		<h1 className="text-center">{children}</h1>
-	</>
+	</PageHeaderWrapper>
 )
 
 export default PageHeader;
