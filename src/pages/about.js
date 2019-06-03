b9a4682790aebc90f889ddba1ac39ae9c3212597
Cristian <cristianguerra@rocketmail.com>
2019-06-02 21:02:31 -0400

    About us page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,15 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Container from "../components/container"
 import PageHeader from "../components/page-header"
+import AboutImage from "../images/about.inline.svg"
 
 export default () => (
 	<Layout>
 		<SEO title="About Me" />
 		<Container>
 			<PageHeader>About Me</PageHeader>
-			<p>About page</p>
+			<AboutImage />
+
 		</Container>
 	</Layout>
 )
