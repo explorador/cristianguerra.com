a04a21ab204df957ff3a03569ebb245d3aa189ca
Cristian <cristianguerra@rocketmail.com>
2019-06-25 20:01:05 -0400

    Creating /blog posts from contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,30 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+	const { createPage } = actions;
+	return graphql(`
+		{
+			allContentfulBlogPost {
+				edges {
+					node {
+						id
+						slug {
+							slug
+						}
+					}
+				}
+			}
+		}
+	`).then(result => {
+		result.data.allContentfulBlogPost.edges.forEach(({ node }) => {
+			createPage({
+				path: `/blog/${node.slug.slug}`,
+				component: require.resolve(`./src/templates/blog-post.js`),
+				context: {
+					slug: node.slug.slug,
+				},
+			});
+		});
+	});
+}
