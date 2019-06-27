b7604dcb74a1b16eb4af7c4f5603b29d4124e49e
Cristian <cristianguerra@rocketmail.com>
2019-06-26 23:45:01 -0400

    Adding "PrismJS" plugin (For syntax highlighting) ⚡️

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,36 @@ module.exports = {
 			resolve: `gatsby-source-contentful`,
 			options: contentfulConfig,
 		},
-		`gatsby-transformer-remark`,
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-prismjs`,
+						options: {
+							classPrefix: "language-",
+							inlineCodeMarker: null,
+							aliases: {},
+							showLineNumbers: false,
+							noInlineHighlight: false,
+							languageExtensions: [
+								{
+									language: "superscript",
+									extend: "javascript",
+									definition: {
+										superscript_types: /(SuperType)/,
+									},
+									insertBefore: {
+										function: {
+											superscript_keywords: /(superif|superelse)/,
+										}
+									},
+								}
+							],
+						},
+					},
+				],
+			},
+		},
 	],
 }
