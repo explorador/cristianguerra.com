78bb6055c46fb9f8dbe402ce33184ded84ca764a
Cristian <cristianguerra@rocketmail.com>
2019-06-27 00:58:15 -0400

    Bringing content from "Contentful" for About and Contact pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,41 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, StaticQuery } from "gatsby"
 import SEO from "../components/seo"
 import Container from "../components/container"
 import PageHeader from "../components/page-header"
 import AboutAnimation from "../components/about-animation"
 
 export default () => (
-	<Layout>
-		<SEO title="About Me" />
-		<Container>
-			<PageHeader
-				title="About Me"
-				subtitle="Another “Origin Story”"
-			/>
-			<AboutAnimation />
-		</Container>
-	</Layout>
+	<StaticQuery
+		query={graphql`
+			query {
+				contentfulPage(contentful_id: {eq: "2E8puKoDdPYhuYWo6ejbOr"}) {
+					title
+					subtitle
+					content {
+						childMarkdownRemark {
+							html
+						}
+					}
+				}
+			}
+		`}
+		render={data => {
+			const page = data.contentfulPage;
+			return (
+				<Layout>
+					<SEO title={page.title} />
+					<Container>
+						<PageHeader
+							title={page.title}
+							subtitle={page.subtitle}
+						/>
+						<AboutAnimation />
+						<div dangerouslySetInnerHTML={{ __html: page.content.childMarkdownRemark.html }} />
+					</Container>
+				</Layout>
+			)
+		}}
+	/>
 )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from "react"
-
 import Layout from "../components/layout"
+import { graphql, StaticQuery } from "gatsby"
 import SEO from "../components/seo"
 import Container from "../components/container"
 import PageHeader from "../components/page-header"
@@ -8,16 +8,36 @@ import ProjectList from "../components/projects-list"
 import Stars from "../components/stars"
 
 export default () => (
-	<Layout>
-		<SEO title="Contact" />
-		<Container>
-			<Stars opacity=".8" />
-			<PageHeader
-				title="Contact"
-				subtitle="Where To Find Me"
-			/>
-			<p className="text-center">I’m not available for freelance projects at the moment. <br/>You can follow me on Github.</p>
-		</Container>
-		<ProjectList />
-	</Layout>
+	<StaticQuery
+		query={graphql`
+			query {
+				contentfulPage(contentful_id: {eq: "VzE6RRuGIyrqPOmpK9ifw"}) {
+					title
+					subtitle
+					content {
+						childMarkdownRemark {
+							html
+						}
+					}
+				}
+			}
+		`}
+		render={data => {
+			const page = data.contentfulPage;
+			return (
+				<Layout>
+					<SEO title={page.title} />
+					<Container>
+						<Stars opacity=".8" />
+						<PageHeader
+							title={page.title}
+							subtitle={page.subtitle}
+						/>
+						<div dangerouslySetInnerHTML={{ __html: page.content.childMarkdownRemark.html }} />
+					</Container>
+					<ProjectList />
+				</Layout>
+			)
+		}}
+	/>
 )
