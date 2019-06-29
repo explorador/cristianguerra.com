0541a1db88f8d10b555b938fcab8da1695bf653b
Cristian <cristianguerra@rocketmail.com>
2019-06-28 21:42:51 -0400

    Updating <Link /> component default and :hover states color/transition

--- src/components/link.js
+++ src/components/link.js
@@ -23,7 +23,7 @@ const PlainLink = ({ children, to, activeClassName, partiallyActive, btnStyle, .
 		)
 	}
 	return (
-		<a href={to} {...other}>
+		<a target="_blank" rel="noopener noreferrer" href={to} {...other}>
 			{children}
 		</a>
 	)
@@ -36,19 +36,17 @@ const PlainLink = ({ children, to, activeClassName, partiallyActive, btnStyle, .
 const Link = styled(PlainLink)`
 	${ props => typeof props.btnStyle !== 'undefined' &&`
 		display: inline-block;
-		padding: ${em( 14, props.theme.baseFontSize )} ${em( 25, props.theme.baseFontSize )};
+		padding: ${em( 12, props.theme.baseFontSize )} ${em( 25, props.theme.baseFontSize )};
 		background-color: ${ props.theme[`${props.btnStyle}Color`] };
 		font-weight: 700;
 		text-decoration: none;
 		text-transform: uppercase;
 		line-height: 1.1;
 		border-radius: 5px;
-		box-shadow: 0 0 14px 0 ${ darken( '0.2', props.theme[`${props.btnStyle}Color`] ) };
-		transition: background-color ease .2s;
+		transition: box-shadow ease .2s;
 
 		&:hover {
-			background-color: ${ darken( '0.15', props.theme[`${props.btnStyle}Color`] ) };
-			box-shadow: 0 0 14px 0 ${ darken( '0.3', props.theme[`${props.btnStyle}Color`] ) };
+			box-shadow: 0 0 14px 0 ${ darken( '0.2', props.theme[`${props.btnStyle}Color`] ) };
 		}
 	`}
 `
