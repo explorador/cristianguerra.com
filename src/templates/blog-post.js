f86dcc589ab597ec28fbb6f4df37b924a1182763
Cristian <cristianguerra@rocketmail.com>
2019-06-28 22:02:59 -0400

    Styling <BlogPostHeader /> header for "blog-post" template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,13 +2,28 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Container from "../components/container"
-import PageHeader from "../components/page-header"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import styled from "styled-components"
 import { rem } from "polished"
 import Stars from "../components/stars"
 
+/**
+ * <BlogPostHeader /> styles.
+ */
+const BlogPostHeader = styled.div`
+	padding-top: ${ props => rem( 70 ) };
+	padding-bottom: ${ props => rem( 60 ) };
+
+	h1 {
+		margin-bottom: 0;
+		text-transform: none;
+	}
+`
+
+/**
+ * <BlogPostImage /> styles.
+ */
 const BlogPostImage = styled(Img)`
 	margin-bottom: ${ rem( 60 ) };
 	filter: grayscale(100%);
@@ -21,9 +36,7 @@ export default ({ data }) => {
 			<SEO title={post.title} />
 			<Container>
 				<Stars position="center" opacity="0.5" />
-				<PageHeader
-					title={post.title}
-				/>
+				<BlogPostHeader><h1 className="h3 text-center">{post.title}</h1></BlogPostHeader>
 				<BlogPostImage alt={post.postImage.description} fluid={post.postImage.fluid} />
 				<div dangerouslySetInnerHTML={{ __html: post.content.childMarkdownRemark.html }} />
 			</Container>
