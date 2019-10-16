e9d630a49c049ea09dcf1810b6f65da5dfeff045
Cristian <cristianguerra@rocketmail.com>
2019-10-15 23:11:11 -0400

    Add "Orion" constellation animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import SEO from "../components/seo"
 import HomeHero from "../components/home-hero"
 import ProjectList from "../components/projects-list"
 import BlogPostList from "../components/blog-post-list"
+import Orion from "../components/orion-animation"
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" />
 		<HomeHero />
+		<Orion />
 		<ProjectList />
 		<BlogPostList title="Latest From My Blog" limit={ 3 } />
 	</Layout>
