7de03cd2991b68641f8bf6b8dd9a7c4b1faefcb6
Cristian <cristianguerra@rocketmail.com>
2019-10-19 17:01:49 -0400

    Add className and ability to add styles to all icons

--- src/components/icon.js
+++ src/components/icon.js
@@ -5,12 +5,12 @@ import React from "react"
  * @param string src First name of the  *.inline.svg file.
  * @ref https://www.gatsbyjs.org/packages/gatsby-plugin-react-svg/
  */
-const Icon = ({ src }) => {
+const Icon = ({ src, ...other }) => {
 	try {
 		// Include svg image based on src prop.
 		const SvgIcon = require(`../images/${src}.inline.svg`)
 		// Render new Component.
-		return <SvgIcon />
+		return <SvgIcon className="svg-icon" {...other} />
 	} catch (err) {
 		return <>Icon Not Found</>
 	}
